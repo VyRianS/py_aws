dddee2a3a834c1269abbbf4d1e2996a79d5438c9
Jianyu <vyrians@gmail.com>
2019-05-14 22:16:22 +0800

    else missing a colon

--- s3.py
+++ s3.py
@@ -6,7 +6,7 @@ s3 = boto3.resource('s3')
 
 if len(list(s3.buckets.all())) == 0:
     print('WARNING: No buckets available.')
-else
+else:
     for bucket in s3.buckets.all():
         print(bucket.name)
 
