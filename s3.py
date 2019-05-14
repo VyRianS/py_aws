ed90673566da9f3f917548a78b32319059dc2b32
Jianyu <vyrians@gmail.com>
2019-05-14 23:25:43 +0800

    list bucket, object, and download functions added

--- s3.py
+++ s3.py
@@ -1,12 +1,55 @@
 #!/usr/bin/python3.6
 
+import os
 import boto3
 
 s3 = boto3.resource('s3')
 
-if len(list(s3.buckets.all())) == 0:
-    print('WARNING: No buckets available.')
-else:
-    for bucket in s3.buckets.all():
-        print(bucket.name)
+def s3_list_buckets():
+    if len(list(s3.buckets.all())) == 0:
+        print('WARNING: No S3 buckets available.')
+        return 1
+    else:
+        bucket_list = []
+        for bucket in s3.buckets.all():
+            print(bucket.name)
+            bucket_list.append(bucket)
+        return bucket_list
 
+def s3_list_bucket_objects(bucket_name):
+    if len(list(s3.Bucket(bucket_name).objects.all())) == 0:
+        print('WARNING: No objects in S3 bucket \'' + bucket_name + '\'.')
+        return 1
+    else:
+        object_list = []
+        for object in s3.Bucket(bucket_name).objects.all():
+            print(object.key)
+            object_list.append(object.key)
+        return object_list
+
+def s3_download_bucket_object(bucket_name, object_name, output_path, overwrite = False):
+    if os.path.isdir(os.path.split(output_path)[0]) == False:
+        print('FATAL: Local path \'' + os.path.split(output_path)[0] + ' does not exist.')
+        return 1
+
+    if os.path.isfile(output_path):
+        if overwrite == False:
+            print('FATAL: Local file \'' + output_path + '\' already exists.')
+            return 1
+
+    # Iterates through all the objects to find object_name.
+    for object in s3.Bucket(bucket_name).objects.all():
+        if object.key == object_name:
+            s3.Object(bucket_name, object_name).download_file(output_path)
+            print('\'' + object_name + '\' downloaded locally to \'' + output_path + '\'.')
+            return 0
+
+    # Object does not exist if code reaches this point.
+    print('WARNING: Object \'' + object_name + \
+          '\' not found in S3 bucket \'' + bucket_name + '\'.')
+    return 1
+
+if __name__ == '__main__':
+    s3_list_buckets()
+    s3_list_bucket_objects('py-aws-testing')
+    s3_download_bucket_object('py-aws-testing','asd.txt','/home/code/asd.txt')
