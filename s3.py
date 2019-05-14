72c433c6ba46292a940f8ae4b1f36dc1de6ebdf9
Jianyu <vyrians@gmail.com>
2019-05-14 21:57:03 +0800

    initial

---
+++ s3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.6
+
+import boto3
+
+s3 = boto3.resource('s3')
+
+if len(list(s3.buckets.all())) == 0:
+    print('WARNING: No buckets available.')
+else
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+
